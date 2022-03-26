f46791de42a1c67d2cf6eff00c9bdce0a95fa6d7
Kyle San Clemente <kylesanclemente@gmail.com>
2022-03-25 17:04:50 -0700

    Add Tailwind.css

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :discustimation, DiscustimationWeb.Endpoint,
   secret_key_base: "W5bL1kY5xnsNIvTZdDonTmoSLbSERAvYgNPkg4C4TL+fbezYzByrL++QR1Ij3Ye+",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
